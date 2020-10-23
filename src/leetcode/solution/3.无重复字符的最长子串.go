4d03cba821a7aa7787d6c0b6ad7423f21e4514bd
derekuang <k2497808365@gmail.com>
2020-10-23 17:15:54 +0800

    Add solution of problem[3](v1).

---
+++ src/leetcode/solution/3.无重复字符的最长子串.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=golang
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+package solution
+
+// @lc code=start
+func lengthOfLongestSubstring(s string) int {
+	if len(s) <= 1 {
+		return len(s)
+	}
+
+	res, cnts := 0, map[byte]int{}
+	for l, r := 0, 0; r < len(s); r++ {
+		cnts[s[r]]++
+		if cnts[s[r]] == 1 {
+			if r-l+1 > res {
+				res++
+			}
+		} else {
+			for cnts[s[r]] > 1 {
+				cnts[s[l]]--
+				l++
+			}
+		}
+	}
+
+	return res
+}
+
+// @lc code=end
