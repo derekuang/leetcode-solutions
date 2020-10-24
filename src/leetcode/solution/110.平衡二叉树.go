a5346aa82745c015036800b3d3787827d9f88dfa
derekuang <k2497808365@gmail.com>
2020-10-24 13:28:34 +0800

    Add the solution of problem[110](v1).

---
+++ src/leetcode/solution/110.平衡二叉树.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=golang
+ *
+ * [110] 平衡二叉树
+ */
+
+package solution
+
+import "math"
+
+// @lc code=start
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	lDepth := getDepth(root.Left)
+	rDepth := getDepth(root.Right)
+	if math.Abs(float64(lDepth-rDepth)) > 1 {
+		return false
+	}
+	return isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func getDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	lDepth := getDepth(root.Left)
+	rDepth := getDepth(root.Right)
+	if lDepth > rDepth {
+		return lDepth + 1
+	}
+	return rDepth + 1
+}
+
+// @lc code=end
