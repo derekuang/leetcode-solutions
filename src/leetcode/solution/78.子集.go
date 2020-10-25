ba8fe8262d3a7ca1206d9937ba34c5c17c8d09bd
derekuang <k2497808365@gmail.com>
2020-10-25 22:33:51 +0800

    Add the solution of problem[78](v2/100%/11.57%).

--- src/leetcode/solution/78.子集.go
+++ src/leetcode/solution/78.子集.go
@@ -7,41 +7,19 @@
 package solution
 
 // @lc code=start
-func subsets(nums []int) [][]int {
+func subsets(nums []int) (res [][]int) {
 	len := len(nums)
-	res := [][]int{}
-	res = append(res, []int{})
-	// Get the [i]int{} array
-	for i := 1; i <= len; i++ {
-		// Pointer list save every subscript of nums that make up the new set
-		pList := make([]int, i)
-		for j := 0; j < i; j++ {
-			pList[j] = j
-		}
-		// The last sub set of [i]int{} must be nums[len-i : len]
-		for pList[0] <= len-i {
-			newSet := make([]int, i)
-			// Assign value to the new set
-			for j, index := range pList {
-				newSet[j] = nums[index]
-			}
-			res = append(res, newSet)
-			// Get the subscripts of next set with backtrack
-			p := i - 1
-			for p >= 0 && pList[p] == len+p-i {
-				p--
-			}
-			if p >= 0 {
-				pList[p]++
-				for p++; p < i; p++ {
-					pList[p] = pList[p-1] + 1
-				}
-			} else {
-				pList[0] = len
+	for mask := 0; mask < (1 << len); mask++ {
+		set := []int{}
+		for i, num := range nums {
+			if (mask>>i)&1 > 0 {
+				// num should present in the set
+				set = append(set, num)
 			}
 		}
+		res = append(res, append([]int(nil), set...))
 	}
-	return res
+	return
 }
 
 // @lc code=end
