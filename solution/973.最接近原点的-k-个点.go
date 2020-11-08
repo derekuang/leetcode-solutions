604ec7725dd4440c7fc1679cb5f6da8d64e8e870
derekuang <k2497808365@gmail.com>
2020-11-09 01:09:25 +0800

    Add the solution of problem[973](v3/52.5%/37.98%).

--- solution/973.最接近原点的-k-个点.go
+++ solution/973.最接近原点的-k-个点.go
@@ -6,41 +6,15 @@
 
 package solution
 
+import "sort"
+
 // @lc code=start
-type pointList struct {
-	index int
-	dist  int
-	next  *pointList
-}
-
-func kClosest(points [][]int, K int) (ans [][]int) {
-	pHead := &pointList{-1, 0, nil}
-
-	pow := func(x int) int {
-		return x * x
-	}
-	for i, point := range points {
-		d := pow(point[0]) + pow(point[1])
-		p, q := pHead, &pointList{i, d, nil}
-		acc := 0
-		for p.next != nil && acc < K {
-			if p.next.dist < d {
-				p = p.next
-				acc++
-			} else {
-				break
-			}
-		}
-		q.next = p.next
-		p.next = q
-	}
-
-	p := pHead.next
-	for i := 0; i < K; i++ {
-		ans = append(ans, append([]int{}, points[p.index]...))
-		p = p.next
-	}
-	return
+func kClosest(points [][]int, k int) [][]int {
+	sort.Slice(points, func(i, j int) bool {
+		p, q := points[i], points[j]
+		return p[0]*p[0]+p[1]*p[1] < q[0]*q[0]+q[1]*q[1]
+	})
+	return points[:k]
 }
 
 // @lc code=end
