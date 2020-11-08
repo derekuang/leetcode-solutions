9094ac797bb7c051fdbcfd0e4c4fbc47e24e270d
derekuang <k2497808365@gmail.com>
2020-11-09 00:56:18 +0800

    Add the solution of problem[973](v2/6.25%/12.66%).

--- solution/973.最接近原点的-k-个点.go
+++ solution/973.最接近原点的-k-个点.go
@@ -7,27 +7,38 @@
 package solution
 
 // @lc code=start
+type pointList struct {
+	index int
+	dist  int
+	next  *pointList
+}
+
 func kClosest(points [][]int, K int) (ans [][]int) {
-	ans = make([][]int, K)
-	for i := 0; i < K; i++ {
-		ans[i] = append([]int{}, 10000, 10000)
-	}
+	pHead := &pointList{-1, 0, nil}
 
 	pow := func(x int) int {
 		return x * x
 	}
-	for _, p := range points {
-		dist := pow(p[0]) + pow(p[1])
-		for i, v := range ans {
-			iDist := pow(v[0]) + pow(v[1])
-			if dist < iDist {
-				for j := K - 1; j > i; j-- {
-					ans[j] = ans[j-1]
-				}
-				ans[i] = append([]int{}, p[0], p[1])
+	for i, point := range points {
+		d := pow(point[0]) + pow(point[1])
+		p, q := pHead, &pointList{i, d, nil}
+		acc := 0
+		for p.next != nil && acc < K {
+			if p.next.dist < d {
+				p = p.next
+				acc++
+			} else {
 				break
 			}
 		}
+		q.next = p.next
+		p.next = q
+	}
+
+	p := pHead.next
+	for i := 0; i < K; i++ {
+		ans = append(ans, append([]int{}, points[p.index]...))
+		p = p.next
 	}
 	return
 }
