df9ae06e2ab1b5a925af920a13d2329516a13f43
derekuang <k2497808365@gmail.com>
2020-11-09 12:53:26 +0800

    Add the solution of problem[973](v4/92.41%/57.69%).

--- solution/973.最接近原点的-k-个点.go
+++ solution/973.最接近原点的-k-个点.go
@@ -6,14 +6,42 @@
 
 package solution
 
-import "sort"
-
 // @lc code=start
 func kClosest(points [][]int, k int) [][]int {
-	sort.Slice(points, func(i, j int) bool {
-		p, q := points[i], points[j]
-		return p[0]*p[0]+p[1]*p[1] < q[0]*q[0]+q[1]*q[1]
-	})
+	distance := func(p int) int {
+		return points[p][0]*points[p][0] + points[p][1]*points[p][1]
+	}
+	qSort := func(l, r int) int {
+		pivot := distance(l)
+		for l < r {
+			dr := distance(r)
+			for r > l && dr >= pivot {
+				r--
+				dr = distance(r)
+			}
+			points[l], points[r] = points[r], points[l]
+			dl := distance(l)
+			for l < r && dl <= pivot {
+				l++
+				dl = distance(l)
+			}
+			points[l], points[r] = points[r], points[l]
+		}
+		return l
+	}
+
+	pivot := -1
+	l, r := 0, len(points)-1
+	for pivot != k-1 {
+		if pivot < k-1 {
+			l = pivot + 1
+			pivot = qSort(l, r)
+		} else {
+			r = pivot
+			pivot = qSort(l, r)
+		}
+	}
+
 	return points[:k]
 }
 
