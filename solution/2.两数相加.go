97208e3f0350440faa9a81026cd5b89f8314435a
derekuang <k2497808365@gmail.com>
2020-11-13 10:55:13 +0800

    Add the solution of problem[2](v2/71.66%/22%).

--- solution/2.两数相加.go
+++ solution/2.两数相加.go
@@ -8,33 +8,24 @@ package solution
 
 // @lc code=start
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) (ans *ListNode) {
-	p, p1, p2 := &ListNode{}, l1, l2
-	ad := 0
+	p, p1, p2, ad := &ListNode{}, l1, l2, 0
 	ans = new(ListNode)
 	p.Next = ans
 	for p1 != nil || p2 != nil || ad != 0 {
 		var n1, n2 int
-		if p1 == nil {
-			n1 = 0
-		} else {
-			n1 = p1.Val
-			p1 = p1.Next
+		if p1 != nil {
+			n1, p1 = p1.Val, p1.Next
 		}
-		if p2 == nil {
-			n2 = 0
-		} else {
-			n2 = p2.Val
-			p2 = p2.Next
+		if p2 != nil {
+			n2, p2 = p2.Val, p2.Next
 		}
 		sum := n1 + n2 + ad
-		ad = sum / 10
-		sum %= 10
-		p.Next.Val = sum
-		p.Next.Next = new(ListNode)
+		ad, sum = sum/10, sum%10
+		p.Next.Val, p.Next.Next = sum, new(ListNode)
 		p = p.Next
 	}
 	p.Next = nil
-	return ans
+	return
 }
 
 // @lc code=end
