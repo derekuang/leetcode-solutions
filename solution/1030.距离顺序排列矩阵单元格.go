0554bdb8cb73e059525072c4fa1536884c1c4344
derekuang <k2497808365@gmail.com>
2020-11-17 00:47:13 +0800

    Add the solution of problem[1030](v2/91.8%/28.57%).

--- solution/1030.距离顺序排列矩阵单元格.go
+++ solution/1030.距离顺序排列矩阵单元格.go
@@ -7,15 +7,11 @@
 package solution
 
 // @lc code=start
-type coord struct {
-	x, y int
-}
-
 // BFS
 func allCellsDistOrder(R int, C int, r0 int, c0 int) (ans [][]int) {
-	q := []coord{}
-	q = append(q, coord{r0, c0})
 	dirs := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // up, down, left, right
+	q := [][]int{}
+	q = append(q, []int{r0, c0})
 	visited := make([][]bool, R)
 	for i := range visited {
 		visited[i] = make([]bool, C)
@@ -23,14 +19,13 @@ func allCellsDistOrder(R int, C int, r0 int, c0 int) (ans [][]int) {
 	visited[r0][c0] = true
 
 	for len(q) > 0 {
-		p := q[0]
-		x, y := p.x, p.y
+		x, y := q[0][0], q[0][1]
 		ans = append(ans, []int{x, y})
 		for _, dir := range dirs {
 			newX, newY := x+dir[0], y+dir[1]
 			if newX >= 0 && newX < R &&
 				newY >= 0 && newY < C && !visited[newX][newY] {
-				q = append(q, coord{newX, newY})
+				q = append(q, []int{newX, newY})
 				visited[newX][newY] = true
 			}
 		}
