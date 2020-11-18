823a3582e51d0caad5966d4528da26e419492992
derekuang <k2497808365@gmail.com>
2020-11-19 00:35:41 +0800

    Add the solution of problem[283](v1/94.22%/68.95%).

---
+++ solution/283.移动零.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=golang
+ *
+ * [283] 移动零
+ */
+
+package solution
+
+// @lc code=start
+func moveZeroes(nums []int) {
+	n, p, q := len(nums), 0, 0
+	for p < n-1 {
+		for p < n-1 && nums[p] != 0 {
+			p++
+		}
+		if p >= n-1 {
+			return
+		}
+		if p+1 > q {
+			q = p + 1
+		}
+		for q < n && nums[q] == 0 {
+			q++
+		}
+		if q >= n {
+			return
+		}
+		nums[p], nums[q] = nums[q], nums[p]
+		p++
+		q++
+	}
+	return
+}
+
+// @lc code=end
