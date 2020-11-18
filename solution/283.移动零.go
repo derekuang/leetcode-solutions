2f873dbc6aa9e97c2eecfe090cd052b782e8a854
derekuang <k2497808365@gmail.com>
2020-11-19 00:40:56 +0800

    Add the solution of problem[283](v3/94.22%/68.95%).

--- solution/283.移动零.go
+++ solution/283.移动零.go
@@ -10,23 +10,10 @@ package solution
 func moveZeroes(nums []int) {
 	n, p, q := len(nums), 0, 0
 	for q < n {
-		for p < n-1 && nums[p] != 0 {
+		if nums[q] != 0 {
+			nums[p], nums[q] = nums[q], nums[p]
 			p++
 		}
-		if p >= n-1 {
-			return
-		}
-		if p+1 > q {
-			q = p + 1
-		}
-		for q < n && nums[q] == 0 {
-			q++
-		}
-		if q >= n {
-			return
-		}
-		nums[p], nums[q] = nums[q], nums[p]
-		p++
 		q++
 	}
 	return
