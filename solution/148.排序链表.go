a7728500c23466673c1c94d17140fc7a4227c2c6
derekuang <k2497808365@gmail.com>
2020-11-21 01:21:55 +0800

    Add the solution of problem[148](v3/28.59%/5.83%).

--- solution/148.排序链表.go
+++ solution/148.排序链表.go
@@ -6,69 +6,27 @@
 
 package solution
 
+import "sort"
+
 // @lc code=start
 func sortList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
 
-	len := 0
+	nums := []int{}
 	for p := head; p != nil; p = p.Next {
-		len++
+		nums = append(nums, p.Val)
+	}
+	sort.Ints(nums)
+
+	p := head
+	for _, num := range nums {
+		p.Val = num
+		p = p.Next
 	}
 
-	dummy := &ListNode{Next: head}
-	for subLen := 1; subLen < len; subLen <<= 1 {
-		pre, cur := dummy, dummy.Next
-		for cur != nil {
-			head1 := cur
-			for i := 1; i < subLen && cur.Next != nil; i++ {
-				cur = cur.Next
-			}
-
-			head2 := cur.Next
-			cur.Next = nil
-			cur = head2
-			for i := 1; i < subLen && cur != nil && cur.Next != nil; i++ {
-				cur = cur.Next
-			}
-
-			var q *ListNode
-			if cur != nil {
-				q = cur.Next
-				cur.Next = nil
-			}
-			cur = q
-
-			pre.Next = merge148(head1, head2)
-			for pre.Next != nil {
-				pre = pre.Next
-			}
-		}
-	}
-	return dummy.Next
-}
-
-func merge148(head1, head2 *ListNode) *ListNode {
-	dummy := &ListNode{}
-	d := dummy
-	for head1 != nil && head2 != nil {
-		if head1.Val <= head2.Val {
-			d.Next = head1
-			head1 = head1.Next
-		} else {
-			d.Next = head2
-			head2 = head2.Next
-		}
-		d = d.Next
-	}
-	if head1 != nil {
-		d.Next = head1
-	}
-	if head2 != nil {
-		d.Next = head2
-	}
-	return dummy.Next
+	return head
 }
 
 // @lc code=end
