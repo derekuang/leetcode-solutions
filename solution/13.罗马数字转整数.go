b896a2799927c8e1635aabbf1914e4ccb60a400e
derekuang <k2497808365@gmail.com>
2020-11-21 17:34:09 +0800

    Add the solution of problem[13](v2/78.66%/40.53%).

--- solution/13.罗马数字转整数.go
+++ solution/13.罗马数字转整数.go
@@ -7,51 +7,27 @@
 package solution
 
 // @lc code=start
+var m13 = map[string]int{
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
+}
+
 func romanToInt(s string) (ans int) {
-	n := len(s)
-	for i := 0; i < n; i++ {
-		add := 0
-		switch s[i] {
-		case 'I':
-			if i+1 < n && s[i+1] == 'V' {
-				add = 4
-				i++
-			} else if i+1 < n && s[i+1] == 'X' {
-				add = 9
-				i++
-			} else {
-				add = 1
-			}
-		case 'X':
-			if i+1 < n && s[i+1] == 'L' {
-				add = 40
-				i++
-			} else if i+1 < n && s[i+1] == 'C' {
-				add = 90
-				i++
-			} else {
-				add = 10
-			}
-		case 'C':
-			if i+1 < n && s[i+1] == 'D' {
-				add = 400
-				i++
-			} else if i+1 < n && s[i+1] == 'M' {
-				add = 900
-				i++
-			} else {
-				add = 100
-			}
-		case 'V':
-			add = 5
-		case 'L':
-			add = 50
-		case 'D':
-			add = 500
-		case 'M':
-			add = 1000
+	pre := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		c := s[i : i+1]
+		num := m13[c]
+		if num >= pre {
+			ans += num
+		} else {
+			ans -= num
 		}
-		ans += add
+		pre = num
 	}
 	return
 }
