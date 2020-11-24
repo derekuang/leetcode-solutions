2d9691ce6d28d1733f465bb5969d0959accde791
derekuang <k2497808365@gmail.com>
2020-11-24 11:12:25 +0800

    Update util.go(makeTree function).

--- solution/util.go
+++ solution/util.go
@@ -23,3 +23,26 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func makeTree(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: nums[0]}
+	nodes := make([]*TreeNode, len(nums))
+	nodes[0] = root
+	for i := 1; i < len(nums); i++ {
+		parent := nodes[(i-1)/2]
+		if nums[i] == -1 {
+			continue
+		} else {
+			nodes[i] = &TreeNode{Val: nums[i]}
+			if i%2 == 1 {
+				parent.Left = nodes[i]
+			} else {
+				parent.Right = nodes[i]
+			}
+		}
+	}
+	return root
+}
