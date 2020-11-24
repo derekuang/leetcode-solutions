2bfc63bf601f23b38635150c91e3d61e9f1b5e74
derekuang <k2497808365@gmail.com>
2020-11-24 20:08:20 +0800

    Add the unsolved problem file[36].

---
+++ solution/36.有效的数独.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=36 lang=golang
+ *
+ * [36] 有效的数独
+ */
+
+package solution
+
+// @lc code=start
+func isValidSudoku(board [][]byte) bool {
+	row, column := map[byte]bool{}, map[byte]bool{}
+	square := make([]map[byte]bool, 9)
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+
+		}
+	}
+	return true
+}
+
+// @lc code=end
