338288a313d5e065895d8b8f4d9eb9d74904a988
derekuang <k2497808365@gmail.com>
2020-11-25 01:01:01 +0800

    Add the solution of problem[1370](v2/79.05%/67.62%).

--- solution/1370.上升下降字符串.go
+++ solution/1370.上升下降字符串.go
@@ -6,51 +6,30 @@
 
 package solution
 
-import "sort"
-
 // @lc code=start
-func sortString(s string) (ans string) {
+func sortString(s string) string {
 	sLen := len(s)
-	if sLen < 2 {
-		return s
-	}
-
-	counter := map[int]int{}
+	counter := ['z' + 1]int{}
 	for _, c := range s {
-		counter[int(c)]++
-	}
-	unique := []int{}
-	for c := range counter {
-		unique = append(unique, c)
-	}
-	sort.Ints(unique)
-
-	cnt := 0
-	appendAns := func(i int) {
-		used := map[int]bool{}
-		c := unique[i]
-		if !used[c] && counter[c] > 0 {
-			used[c] = true
-			counter[c]--
-			cnt++
-			ans = ans + string(rune(c))
-		}
+		counter[c]++
 	}
 
-	flag := -1 // -1: find min;	1: find max
-	for cnt < sLen {
-		if flag == -1 {
-			for i := 0; i < len(unique); i++ {
-				appendAns(i)
-			}
-		} else {
-			for i := len(unique) - 1; i >= 0; i-- {
-				appendAns(i)
+	ans := []byte{}
+	for len(ans) < sLen {
+		for c := 'a'; c <= 'z'; c++ {
+			if counter[c] > 0 {
+				counter[c]--
+				ans = append(ans, byte(c))
 			}
 		}
-		flag *= -1
+		for c := 'z'; c >= 'a'; c-- {
+			if counter[c] > 0 {
+				counter[c]--
+				ans = append(ans, byte(c))
+			}
+		}
 	}
-	return
+	return string(ans)
 }
 
 // @lc code=end
