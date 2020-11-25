aad0b975d2fc402e041b64a08b06b9ddca182d12
derekuang <k2497808365@gmail.com>
2020-11-25 10:52:45 +0800

    Add the solution of problem[344](v1/94.82%/85.71%).

--- solution/344.反转字符串.go
+++ solution/344.反转字符串.go
@@ -14,9 +14,7 @@ func reverseString(s []byte) {
 
 	start, end := 0, len(s)-1
 	for start < end {
-		t := s[start]
-		s[start] = s[end]
-		s[end] = t
+		s[start], s[end] = s[end], s[start]
 		start++
 		end--
 	}
