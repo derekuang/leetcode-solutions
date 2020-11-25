f891bd96b79f8b80d9f642564fcd0f2aaabf7339
derekuang <k2497808365@gmail.com>
2020-11-25 11:27:23 +0800

    Add the solution of problem[7](v2/100%/18.77%).

--- solution/7.整数反转.go
+++ solution/7.整数反转.go
@@ -8,20 +8,17 @@ package solution
 
 // @lc code=start
 func reverse(x int) (ans int) {
-	sign := 1
-	if x < 0 {
-		sign = -1
-		x = -x
-	}
+	anchor0, anchor1 := 214748364, -214748364
 	for x != 0 {
 		digit := x % 10
 		x /= 10
-		if ans > 214748364 {
+		if ans > anchor0 || (ans == anchor0 && digit > 7) ||
+			ans < anchor1 || (ans == anchor1 && digit > 8) {
 			return 0
 		}
 		ans = 10*ans + digit
 	}
-	return sign * ans
+	return ans
 }
 
 // @lc code=end
