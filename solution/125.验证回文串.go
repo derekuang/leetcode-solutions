8c92c30afa9933997cb92c71c17d95792827057c
derekuang <k2497808365@gmail.com>
2020-11-25 14:30:59 +0800

    Add the solution of problem[125](v2/73.15%/79.48%).

--- solution/125.验证回文串.go
+++ solution/125.验证回文串.go
@@ -6,38 +6,22 @@
 
 package solution
 
+import "strings"
+
 // @lc code=start
 func isPalindrome(s string) bool {
 	isLegal := func(c byte) bool {
-		if (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
-			(c >= '0' && c <= '9') {
-			return true
-		}
-		return false
-	}
-	abs := func(x, y byte) byte {
-		if x > y {
-			return x - y
-		}
-		return y - x
-	}
-	isEqual := func(b1, b2 byte) bool {
-		if b1 < 'A' && b2 < 'A' && b1 == b2 {
-			return true
-		}
-		if b1 >= 'A' && b2 >= 'A' && (b1 == b2 || abs(b1, b2) == 32) {
-			return true
-		}
-		return false
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')
 	}
 
+	s = strings.ToLower(s)
 	n := len(s)
 	for p, q := 0, n-1; p < q; {
 		for ; p < q && !isLegal(s[p]); p++ {
 		}
 		for ; q > p && !isLegal(s[q]); q-- {
 		}
-		if p < q && !isEqual(s[p], s[q]) {
+		if p < q && s[p] != s[q] {
 			return false
 		}
 		p++
