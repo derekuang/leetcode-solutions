85b2d7acd2bb0ebabb64e6d8468ee790de162c83
derekuang <k2497808365@gmail.com>
2020-11-25 19:57:11 +0800

    Add the solution of problem[28](v2/100%/10.81%).

--- solution/28.实现-str-str.go
+++ solution/28.实现-str-str.go
@@ -6,11 +6,50 @@
 
 package solution
 
-import "strings"
-
 // @lc code=start
+// KMP
 func strStr(haystack string, needle string) int {
-	return strings.Index(haystack, needle)
+	if needle == "" {
+		return 0
+	} else if haystack == "" || len(haystack) < len(needle) {
+		return -1
+	}
+
+	next := getNext(needle)
+	i, j := 0, 0
+	n1, n2 := len(haystack), len(needle)
+	for i < n1 && j < n2 {
+		if j == -1 || haystack[i] == needle[j] {
+			i++
+			j++
+		} else {
+			j = next[j]
+		}
+	}
+
+	if j == n2 {
+		return i - n2
+	}
+	return -1
+}
+
+func getNext(s string) []int {
+	n := len(s)
+	next := make([]int, n)
+	next[0] = -1
+	for p, q := 0, 1; q < n; {
+		if s[p] == s[q] {
+			next[q] = next[p]
+		} else {
+			next[q] = p
+			for p != -1 && s[p] != s[q] {
+				p = next[p]
+			}
+		}
+		q++
+		p++
+	}
+	return next
 }
 
 // @lc code=end
