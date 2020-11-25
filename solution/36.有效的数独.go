9587563cc447a2eb5325aac58b24f76750b6ea2a
derekuang <k2497808365@gmail.com>
2020-11-25 10:10:26 +0800

    Add the solution of problem[36](v1/63.32%/19.6%).

--- solution/36.有效的数独.go
+++ solution/36.有效的数独.go
@@ -8,11 +8,24 @@ package solution
 
 // @lc code=start
 func isValidSudoku(board [][]byte) bool {
-	row, column := map[byte]bool{}, map[byte]bool{}
-	square := make([]map[byte]bool, 9)
+	row, square := make([]map[byte]bool, 9), make([]map[byte]bool, 9)
 	for i := 0; i < 9; i++ {
+		row[i] = map[byte]bool{}
+		square[i] = map[byte]bool{}
+	}
+	for i := 0; i < 9; i++ {
+		column := map[byte]bool{}
 		for j := 0; j < 9; j++ {
-
+			cur := board[i][j]
+			if cur == '.' {
+				continue
+			} else if column[cur] || row[j][cur] || square[i/3*3+j/3][cur] {
+				return false
+			} else {
+				column[cur] = true
+				row[j][cur] = true
+				square[i/3*3+j/3][cur] = true
+			}
 		}
 	}
 	return true
