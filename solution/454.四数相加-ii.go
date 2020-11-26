43cc8c2814d91470847a271c3d13cba4ede9d212
derekuang <k2497808365@gmail.com>
2020-11-27 01:32:50 +0800

    Add the solution of problem[454](limit exceeded).

---
+++ solution/454.四数相加-ii.go
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=454 lang=golang
+ *
+ * [454] 四数相加 II
+ */
+
+package solution
+
+import "sort"
+
+// @lc code=start
+func fourSumCount(A []int, B []int, C []int, D []int) (ans int) {
+	nums := [][]int{A, B, C, D}
+	counters := [4]map[int]int{}
+	for i := 0; i < 4; i++ {
+		counters[i] = map[int]int{}
+		for _, num := range nums[i] {
+			counters[i][num]++
+		}
+	}
+	uniques := [4][]int{}
+	for i, m := range counters {
+		t := []int{}
+		for num := range m {
+			t = append(t, num)
+		}
+		sort.Ints(t)
+		uniques[i] = t
+	}
+
+	for i := 0; i < len(uniques[0]); i++ {
+		n0 := uniques[0][i]
+		c0 := counters[0][n0]
+		for j := 0; j < len(uniques[1]); j++ {
+			n1 := uniques[1][j]
+			c1 := counters[1][n1]
+			for k := 0; k < len(uniques[2]); k++ {
+				n2 := uniques[2][k]
+				c2 := counters[2][n2]
+				for l := 0; l < len(uniques[3]); l++ {
+					n3 := uniques[3][l]
+					c3 := counters[3][n3]
+					sum := n0 + n1 + n2 + n3
+					if sum == 0 {
+						ans += c0 * c1 * c2 * c3
+						break
+					} else if sum > 0 {
+						break
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
+// @lc code=end
